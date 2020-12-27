125d5e7a10eab036ea0a7b768611e4fbd832cc99
Alex Skorulis <skorulis@gmail.com>
2020-12-27 15:29:46 +1100

    Try different parameters

--- Sources/MainAPI/main.swift
+++ Sources/MainAPI/main.swift
@@ -2,12 +2,12 @@ import App
 import Vapor
 
 var env = try Environment.detect()
-
+print("Running main with arguments: \(env.arguments)")
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 defer { app.shutdown() }
 let config = Configure()
-print("Running main with arguments: \(env.arguments)")
+
 if env.arguments.contains("--scheduled") {
     try config.configureFetch(app)
 } else {
